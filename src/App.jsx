c990108562b4ef66661c45af6f493cece8c5fd4e
Niko Pippuri <niko.pippuri@gmail.com>
2025-08-26 18:12:10 +0300

    todo_part_1 ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [task,setTask] = useState("")
-  const [tasks,setTasks] = useState([])
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  const deleteTask = (deleted) => {
+    const withoutRemoved = tasks.filter((item) => item !== deleted);
+    setTasks(withoutRemoved);
+  };
 
   return (
-    <>
-      <div id="container"></div>
+    <div id="container">
       <h3>Todos</h3>
       <form>
-        <input placeholder='Add new task'/>
+        <input
+          placeholder="Add new task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              addTask();
+            }
+          }}
+        />
       </form>
-    <ul></ul>
-    </>
-  )
+      <ul>
+        {tasks.map((item) => (
+          <li>
+            {item}
+            <button className="delete-button" onClick={() => deleteTask(item)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
