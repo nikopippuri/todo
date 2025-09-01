f79956611ac2f9778f244090bbbf8ea5c3b3536f
Niko Pippuri <niko.pippuri@gmail.com>
2025-09-01 09:47:52 +0300

    Added cross-env for environment variable management for windows usage and updated TaskController to include task insertion functionality.

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -1,4 +1,5 @@
-import { selectAllTasks } from "../models/Task.js";
+import { selectAllTasks, insertTask } from "../models/Task.js";
+import { ApiError } from "../helper/apiError.js";
 
 const getTasks = async (req, res, next) => {
   try {
@@ -9,23 +10,20 @@ const getTasks = async (req, res, next) => {
   }
 };
 
-const postTask = async (req, res,next) => {
-    const { task } = req.body
-    console.log("Task to create:", task)
-    try {
+const postTask = async (req, res, next) => {
+  const { task } = req.body;
+  console.log("Task to create:", task);
+  try {
     if (!task || !task.description || task.description.trim().length === 0) {
-    return next(new ApiError('Task description is required', 400))
-    /* const error = new Error('Task description is required')
-    error.status = 400
-    return next(error) */
+      return next(new ApiError('Task description is required', 400));
     }
-    const result = await insertTask(task.description)
+    const result = await insertTask(task.description);
     return res
-    .status(201)
-    .json({ id: result.rows[0].id, description: result.rows[0].description })
-    } catch (error) {
-    return next(error)
-    }
-    }
-    
+      .status(201)
+      .json({ id: result.rows[0].id, description: result.rows[0].description });
+  } catch (error) {
+    return next(error);
+  }
+};
+
 export { getTasks, postTask };
